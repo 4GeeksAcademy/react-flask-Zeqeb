c8a0d940d022c6ba6f88c84b26ea5f9a9fb6ca4d
ZeqeB <85196913+ezebellino@users.noreply.github.com>
2024-10-23 23:39:06 +0000

    realizando cambios

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,39 @@
 from flask_sqlalchemy import SQLAlchemy
+from enum import Enum
 
 db = SQLAlchemy()
 
+class Favorite_Type(Enum):
+    Planet=1
+    People=2
+
 class User(db.Model):
+    __tablename__ ="User"
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    name = db.Column(db.String(100), nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return f'Name: {self.name}'
 
     def serialize(self):
         return {
+            "name": self.name,
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Favorites(db.Model):
+    __tablename__="Favorites"
+    id = db.Column(db.Integer, primary_key=True)
+    favorite_type = db.Column(db.Enum(Favorite_Type), nullable=False)
+    favorite_id = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("User.id")) #ForeignKey se vincula con el nombre de la tabla 
+    user = db.relationship("User")#Mientras que relationship con el nombre de la CLASE!
+
+    def __repr__(self):
+        favorite=str(self.user) + " - " + str(self.favorite_type)+ ": " + str(self.favorite_id)
+        return f'Favorite: {favorite}'
--- src/api/routes.py
+++ src/api/routes.py
@@ -5,18 +5,65 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 
+app = Flask(__name__)
+bcrypt = Bcrypt(app)
 api = Blueprint('api', __name__)
+#CORS(api)
 
-# Allow CORS requests to this API
-CORS(api)
+@api.route('/signup', methods=['POST'])
+def signup_user():
+    body = request.get_json()
+    if not body or 'email' not in body or 'password' not in body or 'username' not in body:
+        return jsonify({"msg": "Por favor complete todos los campos"}), 400
+    if User.query.filter_by(email=body['email']).first() is not None:
+        return jsonify({"msg": "El usuario ya existe"}), 400
+    
+    hashed_password = bcrypt.generate_password_hash(body['password']).decode('utf-8')
+    user = User(email=body['email'], password=hashed_password, is_active=True)
+    db.session.add(user)
+    db.session.commit()
 
+    return jsonify({"msg": "Usuario creado con éxito", "user": user.serialize()}), 201
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/login', methods=['POST'])
+def user_login():
+    body = request.get_json()
+    if not body or 'email' not in body or 'password' not in body:
+        return jsonify({"msg": "Por favor complete todos los campos"}), 400
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+    user = User.query.filter_by(email=body['email']).first()
+    if not user:
+        return jsonify({"msg": "Usuario no encontrado"}), 401
 
-    return jsonify(response_body), 200
+    valid_user = bcrypt.check_password_hash(user.password, body['password'])
+    if not valid_user:
+        return jsonify({"msg": "Contraseña inválida"}), 401
+    token = create_access_token(identity=user.id, additional_claims={"role": "admin"})
+
+    return jsonify({"msg": "Inicio de sesión exitoso", "token": token}), 200
+
+@api.route("/logout", methods=["POST"])
+@jwt_required()
+def user_logout():
+   token_data=get_jwt()
+   token_blocked=TokenBlockedList(jti=token_data["jti"])
+   db.session.add(token_blocked)
+   db.session.commit()
+   return jsonify({"msg":"Session cerrada"})
+
+@api.route('/userinfo', methods=['GET'])
+@jwt_required()
+def user_info():
+    user_id = get_jwt_identity()
+    user = User.query.get(user_id)
+    if not user:
+        return jsonify({"msg": "Usuario no encontrado"}), 404
+
+    return jsonify({
+        "userInfo": user.serialize(),
+        "role": "admin"
+    }), 200
+
